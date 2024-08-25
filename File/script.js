f19d8768132bffee11a418cf50a1a115701f0e3e
epinox0022 <fjhonnymark@gmail.com>
2024-08-25 14:29:56 +0800

    mice

--- File/script.js
+++ File/script.js
@@ -1,6 +1,10 @@
+"Use strict";
+
+// Arithmetic & Comparison
+
 // 1. Arithmetic Operators
 
-let x;
+// let x;
 
 // x = 5 + 5;
 // x = 5 * 5;
@@ -38,10 +42,42 @@ let x;
 
 // x = 2 == 2;
 // x = 2 === "2";
-x = 2 != "2";
-x = 2 !== "2";
-x = 10 > 5;
-x = 10 < 5;
-x = 10 >= 5;
-x = 10 <= 5;
-console.log(x);
+// x = 2 != "2";
+// x = 2 !== "2";
+// x = 10 > 5;
+// x = 10 < 5;
+// x = 10 >= 5;
+// x = 10 <= 5;
+// console.log(x);
+
+// Type Coercion
+
+// let x;
+
+// x = 5 + "5";
+// x = 5 + Number("5");
+// x = 5 * "5";
+// x = 5 + null; // null = 0
+// x = 5 + true; // true = 1 while false = 0
+
+// console.log(x, typeof x);
+
+// Capitalize Challenge
+
+// const myString = "developer";
+// let myNewString;
+// // solution 1
+// myNewString = myString.charAt(0).toUpperCase() + myString.substring().slice(1);
+
+// // Solution 2
+
+// myNewString = myNewString[0].toUpperCase() + myString.substring(1);
+// console.log(myNewString);
+
+// Numbers
+
+let x;
+
+const num = new Number(5);
+
+x = num.toString();
